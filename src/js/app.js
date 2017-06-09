b6402a62ee50827b251a22cf226257b8299eba72
KrzysztofPlesniak <krzysztofplesniak@interia.pl>
2017-06-09 15:14:10 +0200

    Zmiana obslugi eventow najechania na heaer, scrolowania w dol,pokazywania hamburger menu slwem JS zostal mocno uproszczony

--- src/js/app.js
+++ src/js/app.js
@@ -6,25 +6,19 @@ window.addEventListener('load', function() {
 
 	// 3 belki hamburgera
 	var iconBars = document.querySelectorAll('.iconBar');
-	var menuItems = document.querySelector('.menuItems');
+	var menuItems = document.querySelector('header ul');
+	
 
 	hamburgerIcon.addEventListener('click', function () {
 
-			iconBars.forEach(function(iconBar) {
-				iconBar.classList.toggle('showMenu');
-			});
-			
-			if (menuItems.style.height === "") {
-					menuItems.style.height = "250px";
-					menuItems.style.opacity = 1;
-				 }
-			else {
-					menuItems.style.height = "";
-					menuItems.style.opacity = 0;
-			}		
-		}
-	);
-
+		iconBars.forEach(function(iconBar) {
+				iconBar.classList.toggle('changeHambIcon');
+		});
+		
+		menuItems.classList.toggle('showMenuItems');
+	
+		);
+	}
 
 // Blok kodu obsługujacy anhora "Czytaj więcej"  
 
@@ -47,24 +41,27 @@ window.addEventListener('load', function() {
 	var navi = document.getElementById('navi');
 	var scrollDown = document.querySelector('#scrollDown');
 
+
 	// złapanie zdarzenia scrolowania myszą
 	window.addEventListener('scroll', scrollPage);
 	
 	function scrollPage() {
 		
 		var pageY = window.pageYOffset;
-		var paeWidth = window.outerWidth;
+		var pageWidth = window.outerWidth;
 		//dla ekranów większych niż tablety i smartfony
-		if (pageWidth > 1300) {
+		if (pageWidth > 1280) {
 		
-			if (pageY > 75) { // gdy ekran zjedzie o więcej jak 125px to zmniejsz wysokosc headera o 40px  
-				header.style.height = '100px';
-				navi.style.marginTop = '5px';
-				scrollDown.style.display = 'none';
+			if (pageY > 75) { // gdy nastąpi pierwszy scroll myszką to..
+				// header.style.height = '100px';  
+				// navi.style.marginTop = '5px';
+				div.classList.add("headerSmall");
+				scrollDown.style.display = 'none';  // scrollDown znika
 			}	
 			else {
-				header.style.height = '120px';
-				navi.style.marginTop = '15px';
+				// header.style.height = '120px';
+				// navi.style.marginTop = '15px';
+				div.classList.remove("headerSmall");
 				scrollDown.style.display = 'block';
 			}
 			
@@ -79,44 +76,34 @@ window.addEventListener('load', function() {
 	//złapanie zdarzenia polegającego na najechaniu i opuszczeniu hedera	
 	header.addEventListener('mouseenter', headerExpand);
 	header.addEventListener('mouseleave', headerExpand);
+	
+	var div = document.querySelector('header nav');
 
+	
 	function headerExpand(e) {
 		var pageWidth = window.outerWidth;
 		
 
 		if  ((e.type == "mouseenter") && (pageWidth > 1300)) {
-				header.style.height = '120px'; // zwiększ wysokość hedaera do 140px 
-				navi.style.marginTop = '15px'; // zwiększ  margin-top elelementów w hedaer navi = logo + ul
-				menuATags.forEach(function(menuATag, index) {
-					menuATag.style.fontSize = "1.4rem"; // zwiększ  wielkości czcionek	
-				});
+				div.classList.remove("headerSmall");
+
+				// header.style.height = '120px'; // zwiększ wysokość hedaera do 140px 
+				// navi.style.marginTop = '15px'; // zwiększ  margin-top elelementów w hedaer navi = logo + ul
+				// menuATags.forEach(function(menuATag, index) {
+				// 	menuATag.style.fontSize = "1.4rem"; // zwiększ  wielkości czcionek	
+				// });
 				
 			}
 
 		if ((e.type == "mouseleave") && (pageWidth > 1300)) {
-				header.style.height = '100px';
-				navi.style.marginTop = '5px';  
-				menuATags.forEach(function(menuATag) {
-					menuATag.style.fontSize = "1.3rem"; 
-				});
+				div.classList.add("headerSmall");
+				// header.style.height = '100px';
+				// navi.style.marginTop = '5px';  
+				// menuATags.forEach(function(menuATag) {
+				// 	menuATag.style.fontSize = "1.3rem"; 
+				// });
 			}	
 	}
 
-// Blok kodu zmiany wysokości headera przy zmianie szerokości ekranu
-  window.addEventListener('resize', WindowReSize);
-
-  function WindowReSize() {
-	var pageWidth = window.outerWidth;
-
-	if ((pageWidth > 480) && (pageWidth < 1024)){
-		menuItems.style.opacity = 1;
-		menuItems.style.height = "210px";
-	}
-	else {
-		menuItems.style.opacity = 1;
-		menuItems.style.height = "";	
-	} 
- 	
-  }
   
 }); 

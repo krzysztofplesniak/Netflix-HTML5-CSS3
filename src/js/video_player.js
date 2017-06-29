35558a9c971d44d6f2381cc825cbcdadeaf72c07
KrzysztofPlesniak <krzysztofplesniak@interia.pl>
2017-06-29 16:27:59 +0200

    Poprawki do inputy Szukja, przebudowanie funcki od wyszukiwania w bazie po tytule i ID

--- src/js/video_player.js
+++ src/js/video_player.js
@@ -21,7 +21,6 @@ var btnWatchVideo = document.querySelector('.btnWatchVideo'),
 	
 	
 	function initMovies() {
-
 		var randomMovie;
 		// pobranie polecanych filmów, które są zaczytywane z lokalnego pliku JSON
   		$.getJSON('src/omdb_id.json')
@@ -37,117 +36,71 @@ var btnWatchVideo = document.querySelector('.btnWatchVideo'),
 			});
  		}	
 	
+	
 	// pobranie rozszerzonej ilości danych o jednym filmie tj. tytuł, rok, reżyser,
 	// rok, aktorzy itp...na bazie zapytania do bazy OMDB z wykorzystaniem
 	// id z plik JSON
-	function searchMovieById(movieID) {
+	function searchMovie(type, requestedData) {
+	
+	var movieDetails, requestUrl;
+
+		var request,
+			baseUrl = 'https://www.omdbapi.com/',
+			apiKey = '&apikey=3a2d81a4';
 		
-		var MovieDetails = new Object();
-		
+
+		if (type === 'ID') { 
+				request = '?i='
+			}
+		else if (type === 'Title') {
+				request = '?t='
+			}
+
+		requestURl = baseUrl + request + requestedData + apiKey;
+		console.log(requestURl);
+
 		$.ajax({
-			url: 'https://www.omdbapi.com/?i='+ movieID +'&apikey=3a2d81a4',
+			url: requestURl,
 			type:'GET',
         	dataType:'json',
         	async: false,
-        	success: function(data){
-            	
-            	MovieDetails.Title = data.Title;
-				MovieDetails.DescriptionPart1 = data.Plot; 	
-				MovieDetails.Director = data.Director;			
-				MovieDetails.Genre = data.Genre;
-				MovieDetails.Year = data.Year;
-				MovieDetails.Runtime = data.Runtime;
-				MovieDetails.Poster = data.Poster;
-				MovieDetails.Writer = data.Writer;
-				MovieDetails.Actors = data.Actors;
-				MovieDetails.Awards = data.Awards;
-				MovieDetails.Rating = data.imdbRating;
-
-				// if (MovieDetails.DescriptionPart1.length > 400) {
-				// 		MovieDetails.DescriptionPart1 = data.Plot.slice(0,400);
-				// 		MovieDetails.DescriptionPart2 = data.Plot.slice(400,data.Plot.length);
-				// } 
-				// else {
-				// 		MovieDetails.DescriptionPart2 = '';
-				// }
-				// console.log('OMDB ',data.Title);
-			}
-			
+        	success: function (response) {
+        	 	movieDetails = response;	
+        		}
         });
-		
-        return  MovieDetails; // przekazanie obiektu na zewnątrz danymi do wybranego filmem 
-			
-	}
-
-	function searchMovieByTitle(movieTitle) {
-		
-		var MovieDetails = new Object();
-		
-		$.ajax({
-			url: 'https://www.omdbapi.com/?s='+ movieTitle +'&apikey=3a2d81a4',
-			type:'GET',
-        	dataType:'json',
-        	async: false,
-        	success: function(response){
-        		var data = response.Search[0];
-            	console.log('film tytuł', data);
-
-            	MovieDetails.Title = data.Title;
-				MovieDetails.DescriptionPart1 = data.Plot; 	
-				MovieDetails.Director = data.Director;			
-				MovieDetails.Genre = data.Genre;
-				MovieDetails.Year = data.Year;
-				MovieDetails.Runtime = data.Runtime;
-				MovieDetails.Poster = data.Poster;
-				MovieDetails.Writer = data.Writer;
-				MovieDetails.Actors = data.Actors;
-				MovieDetails.Awards = data.Awards;
-				MovieDetails.Rating = data.imdbRating;
-
-				// if (MovieDetails.DescriptionPart1.length > 400) {
-				// 		MovieDetails.DescriptionPart1 = data.Plot.slice(0,400);
-				// 		MovieDetails.DescriptionPart2 = data.Plot.slice(400,data.Plot.length);
-				// } 
-				// else {
-				// 		MovieDetails.DescriptionPart2 = '';
-				// }
-				
-			}
-			
-        });
-		
-        return  MovieDetails; // przekazanie obiektu na zewnątrz z danymi do wpisanego w pole Szukaj filmem 
-			
-	}
+        
+        return movieDetails
+    }
 
 	
 	// wyświetlenie jednego wybranego filmu w descirptionBOX 
-	function displayDescriptionBox(movie) {
-			
+	function displayDescriptionBox(movieToDisplay) {
+	var movieDetails;
+
 		// zapytanie AJAX'owe do bazy OMBd z użyciem unikalne ID filmu na bazie wylosowanego filmu.
 		// randomMovie ma jedną ze składowych randomMovieID, która jest unikalnym ID w bazie OMDb,
 		// wykorzystanym do odpytania bazy o więcej danych o tym filmie
 
-		var movieDetails = searchMovieById(movie.id);
+		movieDetails = searchMovie('ID', movieToDisplay.id);
 
 		// wklejenie w HTML descriptionBox o wybranym losowym filmie
+
 		$('.filmTitle').text(movieDetails.Title);
-		$('.filmDescriptionP1').text(movieDetails.DescriptionPart1);
-		$('.filmDescriptionP2').text(movieDetails.DescriptionPart2);
+		$('.filmDescriptionP1').text(movieDetails.Plot);
+		// $('.filmDescriptionP2').text(movieDetails.descriptionPart2);
 	    $('.filmGenre').text(movieDetails.Genre);
 	    $('.filmDirector').text(movieDetails.Director);
 	    $('.filmYear').text(movieDetails.Year);
 	    $('.filmRuntime').text(movieDetails.Runtime);
 		$('.filmWriter').text(movieDetails.Writer);
 		$('.filmActors').text(movieDetails.Actors);
-		$('.filmAwards').text(movieDetails.Awards);
-		$('.filmRating').text(movieDetails.Rating);
+		$('.filmRating').text(movieDetails.Arating);
 		$('.filmPoster').attr('src',movieDetails.Poster);
-	    $('#readAboutFilm').attr('href', "http://www.imdb.com/title/"+ movie.id +"/");
+	    $('#readAboutFilm').attr('href', "http://www.imdb.com/title/"+ movieToDisplay.id +"/");
 
 	    // wklejenie w tag <video> danych filmu, kóry ma sie wyśietlic w tle
 	    $('#videoBackground').attr('poster',movieDetails.Poster);
-	    $('#videoBackground').children().attr('src', movie.url);
+	    $('#videoBackground').children().attr('src', movieToDisplay.url);
 	    
 	    // console.log($('#videoBackground').children().attr('src'));
 	    
@@ -166,15 +119,15 @@ var btnWatchVideo = document.querySelector('.btnWatchVideo'),
 	
 	// wyświetlenie pozostałych filmów w tzw. kafelkach videoBox,
 	// tytuł & poster ze zdjęciem z filmu
-	function displayVideoBoxes(movie) {
+	function displayVideoBoxes(movieToDisplay) {
 		
 		var outputHtml, movieDetails;
 	
-		for (i=0; i < movie.length; i++) {
+		for ( i=0; i < movieToDisplay.length; i++) {
 			
 			// pobranie rozszerzonych danych o filmie z zewnetrznej bazy OMDb 
 			// w zmnienniej movie są tylko podstawowe dane tj. url, id, poster, tytuł i opis
-			movieDetails = searchMovieById(movie[i].id);
+			movieDetails = searchMovie('ID', movieToDisplay[i].id);
 		
 			outputHtml = '';
 			outputHtml += '<div class="videoBox">';
@@ -201,14 +154,12 @@ var btnWatchVideo = document.querySelector('.btnWatchVideo'),
 
 	// wybranie losowo jednego filmu z listy 20 filmów 
 	function randomMovies(movies) {
-	
-		var randomMovie = Math.floor(Math.random() * 20);
-		return movies[randomMovie];  // zwracany jest jeden filmów z listy 20 
+		return movies[Math.floor(Math.random() * 20)];  // zwracany jest jeden filmów z listy 20 
 	}
 
 	// funkcja obsługująca nasłuch na elementy związane z Video 
 	function eventListenerVideoBox() {
-			 
+
 		// event reakcja na naciśnięcie klawisza PLAY
 		// var videoPlay = document.querySelector('.fa-play-circle');
 		// videoPlay.addEventListener('click', function() {
@@ -222,21 +173,34 @@ var btnWatchVideo = document.querySelector('.btnWatchVideo'),
 			displayMovie('URL');
 		});
 
-		// event najechania na input "Szukaj"
+		// event kliknięcia na input "Szukaj"
+		menuSearch.addEventListener('click', function() {
+			expandedBox.value = '';
+		});
+		
+		// event kliknięcia na input "Szukaj"
 		menuSearch.addEventListener('mouseenter', function() {
 			smallBox.style.display = 'none';
 			expandedBox.style.display = 'block';
-			menuSearch.classList.add('mouseenter');
-			searchInput();
+			menuSearch.classList.add('mouseSearchClick');
+			expandedBox.value = 'Wpisz film';
 		});
-		
+
 		// event opuszczenia input "Szukaj"
 		menuSearch.addEventListener('mouseleave', function() {
 			smallBox.style.display = 'block';
 			expandedBox.style.display = 'none';
-			menuSearch.classList.remove('mouseenter');
-			expandedBox.value = 'Wpisz ponownie tytuł filmu';	
+			menuSearch.classList.remove('mouseSearchClick');
+			expandedBox.value = 'Wpisz film';	
 		});
+
+		menuSearch.addEventListener('keyup', function(event) {
+				
+			if (event.which == 13 || event.keyCode == 13) {
+				searchInput(expandedBox.value);
+	    	}
+						
+		});	
 	}
 	
 	// modal z trailerem filmu
@@ -245,24 +209,14 @@ var btnWatchVideo = document.querySelector('.btnWatchVideo'),
 	}
 	
 	// obsługa klawisza "Szukaj"
-	function searchInput() {
-		var movieDetails, movieEntered;
+	function searchInput(movieTitle) {
 
-		menuSearch.addEventListener('mousedown', function(event) {
-			expandedBox.value = '';	
-			menuSearch.addEventListener('keyup', function(event) {
+		var movieDetails;
 				
-				if (event.which == 13 || event.keyCode == 13) {
-	     			movieEntered = expandedBox.value;
-	     			console.log('wpisany film to ', movieEntered);
-
-	     			// wywołanie AJAX'owego zapytania do bazy 
-	     			movieDetails = searchMovieByTitle(movieEntered);
-	     			// displayDescriptionBox(movieDetails);
-				}
-				
-			});
-		});	
+		// wywołanie AJAX'owego zapytania do bazy 
+    	movieDetails = searchMovie('Title', movieTitle);
+    	displayDescriptionBox(movieDetails);
+		
 	}
 
 
